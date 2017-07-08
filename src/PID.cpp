a2af38faa9b933d08cf8a07cf785073e2ff3545d
Will Buxton <will.buxton88@gmail.com>
2017-07-08 16:23:37 -0700

    Add files via upload

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,24 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double p, double i, double d, double cte_prev) {
+    Kp = p;
+    Ki = i;
+    Kd = d;
+    cte_pre = cte_prev;
+    cte_tot = 0;
 }
 
 void PID::UpdateError(double cte) {
+    cte_tot += cte;
+    p_error = Kp * cte;
+    i_error = Ki * cte_tot;
+    d_error = Kd * (cte - cte_pre);
+    cte_pre = cte;
+
+
 }
 
 double PID::TotalError() {
+    return -(p_error + i_error + d_error);
 }
-
